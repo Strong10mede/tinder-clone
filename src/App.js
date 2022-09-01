04e039a786044c33317191da356eaeec7c339e27
Strong10mede <kmayur809@gmail.com>
2022-09-01 14:51:07 +0530

    import in app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 import Header from "./Components/Header";
 import TinderCards from "./Components/TinderCards";
 import SwipeButtons from "./Components/SwipeButtons";
-
+import Chats from "./Chats";
+import ChatScreen from "./ChatScreen";
 function App() {
   return (
     <div className="app">
