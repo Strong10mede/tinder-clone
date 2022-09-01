87ad2d105741966c577effbfbb81fbb294223397
Strong10mede <kmayur809@gmail.com>
2022-09-01 14:05:47 +0530

    all files required

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
-import './App.css';
-
+import TinderCards from "./Components/TinderCards";
+import "./App.css";
+import SwipeButtons from "./Components/SwipeButtons";
+import Header from "./Components/Header";
 function App() {
   return (
     <div className="app">
-      
+      <Header />
+      <TinderCards />
+      <SwipeButtons />
     </div>
   );
 }
