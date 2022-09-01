bdde0d07536185c611eca538969bb31f3ddffd98
Strong10mede <kmayur809@gmail.com>
2022-09-01 15:11:53 +0530

    header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,21 +6,31 @@ import ForumIcon from "@mui/icons-material/Forum";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import { Link, useHistory } from "react-router-dom";
 
-function Header() {
+function Header({ backButton }) {
+  const history = useHistory();
   return (
     <div className="header">
-      <IconButton>
-        <PersonIcon fontSize="large" className="header__icon" />
-      </IconButton>
-
-      <img
-        className="header__logo"
-        src="https://1000logos.net/wp-content/uploads/2018/07/Tinder-logo.png"
-        alt=""
-      />
-      <IconButton>
-        <ForumIcon fontSize="large" className="header__icon" />
-      </IconButton>
+      {backButton ? (
+        <IconButton onClick={() => history.replace(backButton)}>
+          <ArrowBackIosIcon className="header__icon" fontSize="large" />
+        </IconButton>
+      ) : (
+        <IconButton>
+          <PersonIcon className="header__icon" fontSize="large" />
+        </IconButton>
+      )}
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+          alt="tinder logo"
+        />
+      </Link>
+      <Link to="/chat">
+        <IconButton>
+          <ForumIcon className="header__icon" fontSize="large" />
+        </IconButton>
+      </Link>
     </div>
   );
 }
