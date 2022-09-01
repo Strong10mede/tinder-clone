1b3a06282d3e34a52fc082693452fbea3a728e86
Strong10mede <kmayur809@gmail.com>
2022-09-01 15:41:39 +0530

    firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD4NYk0PLJoDNc5YMmIB7oTHgm_HTdzuOU",
+  authDomain: "tinder-clone-19c44.firebaseapp.com",
+  projectId: "tinder-clone-19c44",
+  storageBucket: "tinder-clone-19c44.appspot.com",
+  messagingSenderId: "71749600959",
+  appId: "1:71749600959:web:ebdb2b505ff3709da2a80a",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const db = firebaseApp.firestore();
+
+export { db };
