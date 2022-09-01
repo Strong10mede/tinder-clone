79f1b93fab434331530b539b71c8a40e95a4126e
Strong10mede <kmayur809@gmail.com>
2022-09-01 14:49:56 +0530

    app line setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
+import Header from "./Components/Header";
 import TinderCards from "./Components/TinderCards";
 import SwipeButtons from "./Components/SwipeButtons";
-import Header from "./Components/Header";
+
 function App() {
   return (
     <div className="app">
