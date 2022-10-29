d6f4dd3195c2683b54b0c10c5ebe4f696b90c01e
Strong10mede <kmayur809@gmail.com>
2022-10-29 10:49:45 +0530

    all stuff in ContextLayer

---
+++ src/StateLayer.js
@@ -0,0 +1,12 @@
+import { useContext } from "react";
+import { createContext, useReducer } from "react";
+
+const StateContext = createContext();
+
+export const StateProvider = ({ reducer, initialState, children }) => (
+  <StateContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+  </StateContext.Provider>
+);
+
+export const useStateValue = () => useContext(StateContext);
